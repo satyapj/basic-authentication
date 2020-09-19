27d615d315446bd7743a44764fd7c0fe56a88fb1
Satya Prakash Jha <20137460+satyapj@users.noreply.github.com>
2020-09-19 21:45:53 +0530

    Added login/logout in navbar

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { AuthContext } from '../context';
 
 function AppHeader() {
-    return <header>
-        <nav className="navbar navbar-expand navbar-light bg-light">
-            <span className="navbar-brand mb-0 h1">App Navbar</span>
-            <ul className="navbar-nav">
-                <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-                <li className="nav-item"><Link className="nav-link" to="/admin">Admin</Link></li>
-            </ul>
-             
-        </nav>
-        
-    </header>
+  const location = useLocation();
+  const { isAuthenticated, setAuthenticated } = useContext(AuthContext);
+  let logInControls;
+
+  function handleLogout() {
+    setAuthenticated(false);
+  }
+  if (isAuthenticated) {
+    logInControls = <button className="btn btn-link text-secondary" onClick={() => { handleLogout(); }}>Logout</button>;
+  } else {
+    logInControls = <Link to={{ pathname: '/login', state: { from: location.pathname } }}>Login</Link>;
+  }
+  return <header>
+    <nav className="navbar navbar-expand navbar-light bg-light mb-4">
+      <span className="navbar-brand mb-0 h1">App Navbar</span>
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+        <li className="nav-item"><Link className="nav-link" to="/admin">Admin</Link></li>
+        <li className="nav-item"><Link className="nav-link" to="/dashboard">Dashboard</Link></li>
+      </ul>
+      {logInControls}
+    </nav>
+
+  </header>
 }
 
 export default AppHeader;
