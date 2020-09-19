32c998185f5bd2d26d265cd832c2068e5b6e034a
Satya Prakash Jha <20137460+satyapj@users.noreply.github.com>
2020-09-19 21:48:11 +0530

    Updated redirects with state info Added pathname to state info for redirection after successfull login.

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -4,13 +4,13 @@
 
 
 import React, { useContext } from 'react';
-import { Redirect, Route } from "react-router-dom";
-import AuthContext from './context/AuthContext';
+import { Redirect, Route, useLocation } from "react-router-dom";
+import { AuthContext } from './context';
 
 function PrivateRoute({ component: Component, ...rest }) {
   const { isAuthenticated } = useContext(AuthContext);
-  console.log('PrivateRoute -> isAuthenticated: ', isAuthenticated);
-  return <Route {...rest} render={(props) => isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />} />
+  // console.log('PrivateRoute -> isAuthenticated: ', isAuthenticated);
+  return <Route {...rest} render={(props) => isAuthenticated ? <Component {...props} /> : <Redirect to={{ pathname: '/login', state: { from: props.location.pathname } }} />} />
 }
 
 export default PrivateRoute;
